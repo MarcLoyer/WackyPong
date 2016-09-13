49d9e0fcee61ad7173433f93b31f24280b9f114a
MarcLoyer <marcl40@comcast.net>
2016-09-12 19:25:07 -0700

    experimented with collision animation alpha - not effective. I left the code commented-out.

--- core/src/com/obduratereptile/wackypong/world/CollisionAnimation.java
+++ core/src/com/obduratereptile/wackypong/world/CollisionAnimation.java
@@ -4,12 +4,14 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 
 /**
  * Created by Marc on 9/11/2016.
  */
 public class CollisionAnimation extends Actor {
     public final float SCALERATE = 20;
+    public final float ALPHADROP = 1.5f;
 
     World world;
     public float scale = 1;
@@ -20,30 +22,32 @@ public class CollisionAnimation extends Actor {
         world = w;
 
         img = world.game.atlas.createSprite("collision");
-        setSize(radius*2, radius*2);
-        setOrigin(radius, radius);
-        setPosition(x-radius, y-radius);
+        img.setSize(radius*2, radius*2);
+        img.setOrigin(radius, radius);
+        img.setPosition(x-radius, y-radius);
+        img.setScale(1.0f);
     }
 
     @Override
     public void act(float delta) {
         super.act(delta);
-        elapsedTime += Gdx.graphics.getDeltaTime();
+        elapsedTime += delta;
         scale = 1.0f + (elapsedTime * SCALERATE);
         if (scale > 40.0f) {
             remove();
         }
-        setScale(scale);
+        img.setScale(scale);
+
+/*        if (elapsedTime>ALPHADROP) {
+            float a = 1.0f - (elapsedTime - ALPHADROP);
+            if (a < 0) a = 0;
+            img.setAlpha(a);
+        }*/
     }
 
     @Override
     public void draw(Batch batch, float parentAlpha) {
         super.draw(batch, parentAlpha);
-        batch.draw(img,
-                getX(), getY(),
-                getOriginX(), getOriginY(),
-                getWidth(), getHeight(),
-                getScaleX(), getScaleY(),
-                0.0f);
+        img.draw(batch, parentAlpha);
     }
 }
